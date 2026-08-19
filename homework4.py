c91ec6fcc261c2f84858b8f0d3fc15860f5f9573
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-19 16:57:23 +1000

    initial commit

---
+++ homework4.py
@@ -0,0 +1,6 @@
+print ("Welcome to the weather colthes chooser!")
+tempreture = int(input("What tempreture (c*) is it right now?"))
+if tempreture < 23:
+    print ("You should wear a jacket or some thick clothing to avoid the cold weather today!")
+else:
+    print ("You should wear something light and soft to endure the hot weather today!")
