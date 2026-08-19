e713154e84ba26c23ee6a1791571de67b4e52d17
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-19 16:36:43 +1000

    initial commit

---
+++ homework1.py
@@ -0,0 +1,10 @@
+TomBday = 31,5,10
+print ("Tom's birthday is on", TomBday)
+IsaacBday = 19,4,12
+print ("Isaac's birthday is on", IsaacBday)
+samBday = 13,4,11
+print ("Sam's birthday is on", samBday)
+ethanBday = 21,7,13
+print ("Ethan's birthday is on", ethanBday)
+bobBday = 4,12,12
+print ("Bob's birthday is on", bobBday)
