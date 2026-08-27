58c3723f06376d7840496b0bfa7699886b34c942
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-27 18:14:00 +1000

    initial commit

--- AgeBetween10and20.py
+++ AgeBetween10and20.py
@@ -1,6 +1,9 @@
 age = int(input("What is your age?"))
 
-if age >= 10 and age <= 20:
-    print ("You can be enrolled in the class")
+if age >= 10:
+    if age <= 20:
+        print("your age is between 10 and 20")
+    else:
+        print ("your age is greater than 20")
 else:
-    print ("You cannot be enrolled")
+    print ("you age is less than 10 ")
