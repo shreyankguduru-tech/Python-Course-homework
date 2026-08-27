a98f38dfb89e25a00a9bcbd4b00d28411d7804b4
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-27 18:03:54 +1000

    initial commit

---
+++ BirthdayPrinter.py
@@ -0,0 +1,10 @@
+TomBday = "19/12/12"
+print ("Toms birthday is on", TomBday)
+SamBday = "12/4/11"
+print ("Sams birthday is on", SamBday)
+DavidBday = "29/7/13"
+print ("Davids birthday is on", DavidBday)
+IsaacBday = "19/4/11"
+print ("Isaacs birthday is on", IsaacBday)
+EthanBday = "24/8/12"
+print ("Ethans birthday is on", EthanBday)
