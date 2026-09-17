a6d89216d3aba9890fe162d20a5c38d4c482ca12
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-09-17 18:39:31 +1000

    initial commit

---
+++ Mcounter.py
@@ -0,0 +1,4 @@
+text = input("Enter a string:")
+print("total m's in the entered text is", text.count("m"))
+
+
---
+++ MultiplesTill10.py
@@ -0,0 +1,3 @@
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+    print("The multiples for this number is",number * i)
