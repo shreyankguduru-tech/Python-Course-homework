80bd6636da00ef11ad27604fc7b7306199135fdc
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-09-17 16:48:26 +1000

    initial commit

---
+++ CircleCircumference.py
@@ -0,0 +1,13 @@
+import math
+def CircumferenceCalc():
+    pi = math.pi
+
+    radius = int(input("Enter your chosen radius"))
+
+    circumference = 2 * pi * radius
+
+
+    print("The circumference of the circle is", circumference)
+
+
+CircumferenceCalc()
